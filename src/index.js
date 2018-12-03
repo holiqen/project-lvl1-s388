d0f87427fd71f4575fa9e49adb7912b3249309ed
Artem Glushaev <hekill93@mail.ru>
2018-12-03 16:28:26 +0300

    fix 1

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { readlineSync } from 'readline-sync';
+
+const welcome = (gameDis) => {
+  console.log("Welcom to the Brain Games!");
+  const name = readlineSync.question('What is your name: ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+export default welcome;
