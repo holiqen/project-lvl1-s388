66df3302e7a95e611ba5b0b26e73e08066a9d9d5
Artem Glushaev <hekill93@mail.ru>
2018-12-09 21:31:56 +0300

    Fix "Progression" game

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,19 +5,17 @@ const description = 'What number is missing in the progression?';
 const lengthProgression = 10;
 const progression = () => {
   const a = getRandomNumber(0, 100);
-  // let startProg = a;
   let finishedProgression = '';
   const hiddenElementPosition = getRandomNumber(0, lengthProgression);
   const step = getRandomNumber(2, 5);
   const answer = a + (step * hiddenElementPosition);
   for ( let i = 0; i < lengthProgression; i += 1) {
     if (i === hiddenElementPosition) {
-      const startProg = a + (step * i);
-      finishedProgression = `${finishedProgression} ..`;
+      finishedProgression += ` ..`;
     } else {
-      finishedProgression = `${finishedProgression} ${startProg}`;
+      const startProg = a + (step * i);
+      finishedProgression += ` ${startProg}`;
     }
-    // const startProg = a + (step * i);
   }
   const question = finishedProgression.trim();
   const correctAnswer = String(answer);
